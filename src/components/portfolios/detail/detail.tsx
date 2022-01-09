a2909e042e788edf0e2abc6c3377e74932643ae5
joshua <iuttn123@gmail.com>
2022-01-10 01:27:05 +0900

    front-begin

--- src/components/portfolios/detail/detail.tsx
+++ src/components/portfolios/detail/detail.tsx
@@ -18,15 +18,18 @@ const Detail = (props:Iprops) => {
             <div className={styles.content} >
                 <img src={props.content.img} alt=""/>
                 <h2 className={styles.title}>{props.content.title}</h2>
+                <p className={styles.p}>{props.content.p}</p>
                 <div className={styles.tags}>
-                    {props.content.tags.map((value) => (
-                        <img src={`${value}`} alt=""/>
-                    ))}
+                    <h3>Tools</h3>
+                    <div>
+                        {props.content.tags.map((value) => (
+                            <img className={styles.icon} src={`${value}`} alt=""/>
+                        ))}
+                    </div>
                 </div>
-                <p>{props.content.p}</p>
             </div>
-            <div onClick={()=>changeContent(`choice`)} >
-                <h2 className={styles.title}>go to main</h2>
+            <div className={styles.exit} onClick={()=>changeContent(`choice`)} >
+                <h2>go to main</h2>
             </div>
         </>
     );
--- src/components/portfolios/front/front.tsx
+++ src/components/portfolios/front/front.tsx
@@ -9,7 +9,7 @@ interface Iprops{
 const Front = (props:Iprops) => {
 
     const [content, setContent] = useState([
-        {id:`1`,link:`https://www.youtube.com/watch?v=wmllZkygDLc&t=46s`,img:`dream.png`,title:`그림드림`,tags:[`html.png`,`css.png`,`Semantic-UI`,`javascript.png`,`JQuery`,`JAVA`,`Spring Boot`,`JPA`,`Oracle`],p:`아마추어 그림 작가들이 자신의 그림을 판매하는 플랫폼입니다.`},
+        {id:`1`,link:`https://www.youtube.com/watch?v=wmllZkygDLc&t=46s`,img:`dream.png`,title:`그림드림`,tags:[`html.png`,`css.png`,`semantic.png`,`javascript.png`,`jquery.png`,`java.png`,`springboot.png`,`jpa.png`,`oracle.png`],p:`아마추어 그림 작가들이 자신의 그림을 판매하는 플랫폼입니다.`},
         {id:`2`,link:`https://greenie.slabasia.com/`,img:`greenie.png`,title:`Greenie`,tags:[`Kotlin`,`HTML`,`CSS`],p:`greenie 박스를 통해서 현재 코로나 백신 운반을 담당하는 플랫폼입니다.`},
         {id:`3`,link:`https://lovecheat.net/`,img:`lovecheat.png`,title:`LoveCheat`,tags:[`React`,`Kotlin`,`HTML`,`CSS`],p:`이성간의 만남에서 상대방이 의심스러울 때, 그 사람에 관련해서 신상을 찾는 플랫폼입니다.`},
         {id:`4`,link:`https://www.bsidesoft.com/slabbluebasket/joshua/`,img:`marketcoco.png`,title:`Marketcoco`,tags:[`HTML`,`CSS`,`Kotlin`],p:`현지 싱가폴에서 필요한 과일들을 마켓코코 플랫폼을 통해서 구하는 사이트입니다.`},
